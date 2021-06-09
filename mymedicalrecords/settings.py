cea7fd8cc0128d4a75a39a3cb4492bd0361c6d1f
sohail8611 <82253012+sohail8611@users.noreply.github.com>
2021-06-09 01:21:14 -0700

    Update settings.py

--- mymedicalrecords/settings.py
+++ mymedicalrecords/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT=os.path.join('assetProfile')
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
